47f30b1ede49e5358edee2617b5e7d9e52118d12
suzan <asadsuzan7@gmail.com>
2024-11-22 15:59:49 +0600

    define schema and create bike model

---
+++ dist/app/bike/bike.model.js
@@ -0,0 +1,69 @@
+"use strict";
+// import Bike interface modules 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const bike_interface_1 = require("./bike.interface");
+// import core modules
+const mongoose_1 = __importStar(require("mongoose"));
+// Define the Bike schema
+const BikeSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: Object.values(bike_interface_1.BikeCategory),
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+    },
+});
+// Create the bike model
+const BikeModel = mongoose_1.default.model("Bike", BikeSchema);
+exports.default = BikeModel;
---
+++ src/app/bike/bike.model.ts
@@ -0,0 +1,52 @@
+// import Bike interface modules 
+
+import { IBike, BikeCategory } from './bike.interface'
+
+// import core modules
+import mongoose, { Schema, Document, Model } from 'mongoose'
+// Mongoose Document extending the IBike interface
+export interface IBikeDocument extends IBike, Document { }
+
+// Define the Bike schema
+const BikeSchema: Schema<IBikeDocument> = new Schema<IBikeDocument>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: Object.values(BikeCategory),
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+
+// Create the bike model
+const BikeModel: Model<IBikeDocument> = mongoose.model<IBikeDocument>("Bike", BikeSchema)
+
+export default BikeModel
