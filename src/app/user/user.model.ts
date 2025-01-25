fccc97adfa1fc11479df0685103b82d560aa7a6d
asad <asadSuzan7@gmail.com>
2025-01-25 14:50:40 +0600

    refactor user role

--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -5,7 +5,7 @@ const userSchema = new Schema<TUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
-  role: { type: String, default: 'user', enum: ['admin', 'user'] },
+  role: { type: String, default: 'customer', enum: ['admin', 'customer'] },
 });
 
 const User = model<TUser>('User', userSchema);
